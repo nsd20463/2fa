29ae44e23e7d2a4b4f5ef8c47d5255aec6a8977c
Nicolas S. Dade <nic.dade@gmail.com>
2016-07-03 01:28:34 -0700

    found a test vector in google's code. my code doesn't pass yet
    (also found that google's code has a bug: it doesn't zero challenge[]
    before writing the big-endian base-64 vaue into it.

--- 2fa.go
+++ 2fa.go
@@ -16,7 +16,7 @@ package main
 import (
 	"crypto/hmac"
 	"crypto/sha1"
-	"encoding/base64"
+	"encoding/base32"
 	"encoding/binary"
 	"fmt"
 	"log"
@@ -27,47 +27,59 @@ import (
 // TOTP is the heart of the bit twiddling. It
 // computes the current one-time-password given
 // the shared secret with the other side.
-// The shared secret is base-64-encoded.
+// The shared secret is base-32-encoded.
 func TOTP(shared_secret string) string {
-	// convert shared_secret to binary
-	secret, err := base64.StdEncoding.DecodeString(shared_secret)
-	if err != nil {
-		log.Fatalln("Couldn't parse the secret as base64:", err)
-	}
-
 	// compute seconds since the 1st day of our lord of unix
 	seconds := time.Now().Unix()
 
 	// GoogleAuthenticator uses 30-second windows, implemented by
 	// expressing time as the number of 30-second intervals since
 	// the unix zero-time
-	count := uint64(seconds) / 30
+	counter := uint64(seconds) / 30
 
-	// the OTP is built from SHA1_HMAC(key=<given secret>, data=<count as 64-bit big-endian number>)
-	var data [8]byte
-	binary.BigEndian.PutUint64(data[:], count)
-	bytes := hmac.New(sha1.New, secret).Sum(data[:])
+	return TOPT_counter(shared_secret, counter)
+}
+
+// inner part of TOPT() broken out so it can be unit tested
+func TOPT_counter(shared_secret string, counter uint64) string {
+	// convert shared_secret to binary
+	secret, err := base32.StdEncoding.DecodeString(shared_secret)
+	if err != nil {
+		log.Fatalln("Couldn't parse the secret as base32:", err)
+	}
+
+	// the OTP is constructed from HMAC(SHA1, key=<given secret>, data=<counter as 64-bit big-endian value>)
+	data := make([]byte, 8)
+	binary.BigEndian.PutUint64(data, counter)
+
+	hasher := hmac.New(sha1.New, secret)
+	hasher.Write(data)
+	bytes := hasher.Sum(nil)
+
 	// OTP is a uint32 extracted starting at the byte indicated by the lower 4 bits, with the top bit zeroed
-	offset := bytes[sha1.Size-1] & 15
-	otp := uint32(bytes[offset])<<24 + uint32(bytes[offset+1])<<16 + uint32(bytes[offset+2])<<8 + uint32(bytes[offset+3])
+	offset := bytes[sha1.Size-1] & 15 // since SHA1 returns 20 bytes this works without overflowing
+	otp := binary.BigEndian.Uint32(bytes[offset : offset+4])
 	otp &^= 1 << 31
 
-	fmt.Printf("secret = % x\n", secret)
-	fmt.Printf("seconds = %v\n", seconds)
-	fmt.Printf("count = %v\n", count)
+	fmt.Printf("secret = % x (%q)\n", secret, secret)
+	fmt.Printf("counter = %v\n", counter)
 	fmt.Printf("data = % x\n", data)
 	fmt.Printf("bytes = % x\n", bytes)
 	fmt.Printf("offset = %v\n", offset)
 	fmt.Printf("otp = %v\n", otp)
 
-	// the OTP is expressed as a 6 to 8 digit decimal number (think of a PIN)
+	// the OTP is expressed as a 6 digit decimal number (think of a PIN)
 	// the upper digits are discarded
-	return fmt.Sprintf("%08d", otp%1000000)
+	return fmt.Sprintf("%06d", otp%10000)
 }
 
 func main() {
+
+	str := "Hello world....\x00"
+	fmt.Println(str, "=", base32.StdEncoding.EncodeToString([]byte(str)))
+
 	if len(os.Args) < 2 {
-		os.Stderr.WriteString("2fa <base64 shared secret>\n")
+		os.Stderr.WriteString("2fa <base32 shared secret>\n")
 		os.Exit(1)
 	}
 
---
+++ topt_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestTOPT(t *testing.T) {
+	// use the test vector given in the google authenticator PAM library source code in:
+	//  https://github.com/google/google-authenticator/blob/master/libpam/tests/pam_google_authenticator_unittest.c
+
+	secret := "JBSWY3DPEB3W64TMMQXC4LQA" // the base-32-encoded shared secret
+	counter := uint64(10000)             // counter value in uint tests
+	correct := "050548"                  // correct OTP
+
+	otp := TOPT_counter(secret, counter)
+
+	if otp != correct {
+		t.Errorf("TOPT test vector failed. expected TOPT_counter(%v,%v) = %v; got %v", secret, counter, correct, otp)
+	}
+}
