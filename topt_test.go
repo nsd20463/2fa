ae7e642f422525f556977ef7e18108066161b6b7
Nicolas S. Dade <nic.dade@gmail.com>
2016-07-03 02:28:54 -0700

    add test vectors from RFC 6238, which do pass
    I wonder if I am misunderstanding the google test vector somehow

--- topt_test.go
+++ topt_test.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/base32"
 	"testing"
 )
 
-func TestTOPT(t *testing.T) {
+func TestTOPT_google_authenticator(t *testing.T) {
 	// use the test vector given in the google authenticator PAM library source code in:
 	//  https://github.com/google/google-authenticator/blob/master/libpam/tests/pam_google_authenticator_unittest.c
 
@@ -17,4 +18,30 @@ func TestTOPT(t *testing.T) {
 	if otp != correct {
 		t.Errorf("TOPT test vector failed. expected TOPT_counter(%v,%v) = %v; got %v", secret, counter, correct, otp)
 	}
+}
+
+func TestTOPT_RFC6238(t *testing.T) {
+	// use test vectors given in RFC 6238
+
+	secret := base32.StdEncoding.EncodeToString([]byte("12345678901234567890"))
+
+	good := []struct {
+		secs uint64
+		code string
+	}{
+		{59, "94287082"},
+		{1111111109, "07081804"},
+		{1111111111, "14050471"},
+		{1234567890, "89005924"},
+		{2000000000, "69279037"},
+		{20000000000, "65353130"},
+	}
+
+	for _, v := range good {
+		otp := TOPT_inner(secret, v.secs/30, len(v.code))
+		if otp != v.code {
+			t.Errorf("test at secs = %d failed. expected %v; got %v", v.secs, v.code, otp)
+		}
+	}
+
 }
